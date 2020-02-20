43fdb07af052b62ac61db8a670db09d6ceebda29
lilly987 <61200495+lilly987@users.noreply.github.com>
2020-02-20 18:52:04 +0100

    18:51 czwartek

---
+++ Zgadywanka/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra za dużo za mało");
+
+            //1.Komputer losuje
+            Random los = new Random(); // tworze obiekt typu random
+            int wylosowana = los.Next(1,101);
+            Console.WriteLine(wylosowana);
+            Console.WriteLine("wylosowalem liczbe od 1 do 100. \nOdgadnij ja!");
+
+            //2.Człowiek proponuje
+            Console.Write("Podaj swoja propozycje: ");
+            int propozycja = int.Parse(Console.ReadLine());
+
+            //3.Komp ocenia
+            if(propozycja < wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("za mało!");
+                Console.ResetColor();
+            }
+            else if( propozycja > wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Za dużo!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("trafiono");
+                Console.ResetColor();
+            }
+        }
+    }
+}
